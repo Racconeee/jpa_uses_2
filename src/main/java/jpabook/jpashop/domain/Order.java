d18e404d0fb508d9d75c8f5e8ed30d30dd9300aa
Racconeee <raccon6614@gmail.com>
2023-07-21 11:32:21 +0900

    Raccon

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -37,14 +37,13 @@ public class Order {
     private OrderStatus status; //주문상태[ORDER , CANCEL]
 
     //연관관계 메서드
-
     public void setMember(Member member){
         this.member = member;
         member.getOrders().add(this);
     }
 
     public void addOrderItem(OrderItem orderItem){
-        orderItem.add(orderItem);
+        orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
